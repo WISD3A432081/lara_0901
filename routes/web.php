64a213462fb01a2ff684dd08cb46e540954170d8
3A432081 <s707071127@gmail.com>
2017-12-02 23:46:35 +0800

    修改route

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,14 @@ Route::get('/', ['as' => 'home.index' , 'uses' => 'HomeController@index']);
 Route::get('posts', ['as' => 'posts.index', 'uses' => 'PostsController@index']);
 Route::get('posts/{id}', ['as' => 'posts.show' , 'uses' => 'PostsController@show']);
 
-Route::post('posts', ['as' => 'admin.posts.store', 'uses' =>'AdminPostsController@store']);
-Route::patch('posts/{id}', ['as' => 'admin.posts.update', 'uses' =>'AdminPostsController@update']);
-Route::delete('posts/{id}', ['as' => 'admin.posts.destroy', 'uses' =>'AdminPostsController@destroy']);
+
 // 後台
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/', ['as' => 'admin.dashboard.index', 'uses' => 'AdminDashboardController@index']);
     Route::get('posts', ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
     Route::get('posts/create', ['as' => 'admin.posts.create', 'uses' => 'AdminPostsController@create']);
     Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
+    Route::post('posts', ['as' => 'admin.posts.store', 'uses' =>'AdminPostsController@store']);
+    Route::patch('posts/{id}', ['as' => 'admin.posts.update', 'uses' =>'AdminPostsController@update']);
+    Route::delete('posts/{id}', ['as' => 'admin.posts.destroy', 'uses' =>'AdminPostsController@destroy']);
 });
