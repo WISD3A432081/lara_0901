bdc630d6de87caaf9154b951b5646170d549a8b9
3A432081 <s707071127@gmail.com>
2017-12-02 23:17:34 +0800

    練習4-3 開啟新增的路由

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,15 @@
 |
 */
 
-Route::get('/'         , ['as' => 'home.index' , 'uses' => 'HomeController@index']);
-Route::get('posts'     , ['as' => 'posts.index', 'uses' => 'PostsController@index']);
+Route::get('/', ['as' => 'home.index' , 'uses' => 'HomeController@index']);
+Route::get('posts', ['as' => 'posts.index', 'uses' => 'PostsController@index']);
 Route::get('posts/{id}', ['as' => 'posts.show' , 'uses' => 'PostsController@show']);
 
+Route::post('posts', ['as' => 'admin.posts.store', 'uses' =>'AdminPostsController@store']);
 // 後台
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/', ['as' => 'admin.dashboard.index', 'uses' => 'AdminDashboardController@index']);
-
-    Route::get('posts'          , ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
-    Route::get('posts/create'   , ['as' => 'admin.posts.create', 'uses' => 'AdminPostsController@create']);
+    Route::get('posts', ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
+    Route::get('posts/create', ['as' => 'admin.posts.create', 'uses' => 'AdminPostsController@create']);
     Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
 });
